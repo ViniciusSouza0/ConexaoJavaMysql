084d89bacadd73f4e603676ddfe7bc7b569f05d5
aluno <vinicius.s.silveira.2015@gmail.com>
2024-06-06 21:25:25 -0300

    Arquivo Conexao.java Foi modificado o nome do banco de dados na url usada para conectar o software ao banco de dados mySQL local

--- src/main/java/com/mycompany/game1/Conexao.java
+++ src/main/java/com/mycompany/game1/Conexao.java
@@ -1,26 +1,31 @@
 package com.mycompany.game1;
+
 import java.sql.Connection; //serve para fazer a conexao
 import java.sql.DriverManager; //biblioteca
 import java.sql.SQLException; //serve para informar erros
 
+public class Conexao {
 
-public class Conexao{
-    private static final String url = "jdbc:mysql://localhost:3306/jogo";
-    
-    private static final String user = "root";
-    private static final String password = "";
-    
+    private static final String url = "jdbc:mysql://localhost:3306/rpg";
+
+    private static final String user = "rpg";
+    private static final String password = "senacrs";
+
     private static Connection conn;
-    
-    public static Connection getConexao() throws SQLException{
+
+    public static Connection getConexao() throws SQLException {
         try {
-            if(conn == null){
+            System.out.println("URL: " + url);
+            System.out.println("USER: " + user);
+            System.out.println("PASS: " + password);
+            if (conn == null) {
                 conn = DriverManager.getConnection(url, user, password);
                 return conn;
-            }else{
+
+            } else {
                 return conn;
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
